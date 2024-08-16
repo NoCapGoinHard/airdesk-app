92bddb5db5f802648d12ae8004dcd118170f71f4
allozziotharo <01fgermano@gmail.com>
2024-08-16 15:52:35 +0200

    commit iniziale

---
+++ src/main/java/it/airdesk/airdesk_app/AirdeskAppApplication.java
@@ -0,0 +1,13 @@
+package it.airdesk.airdesk_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdeskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdeskAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/airdesk/airdesk_app/AirdeskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.airdesk.airdesk_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirdeskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
