675d3e9758f0d985a7a43fc6513632a9a5f724ff
allozziotharo <01fgermano@gmail.com>
2024-08-23 00:19:53 +0200

    IMPLEMENTAZIONE index

--- src/main/java/it/airdesk/airdesk_app/controller/FacilityController.java
+++ src/main/java/it/airdesk/airdesk_app/controller/FacilityController.java
@@ -1,5 +1,8 @@
 package it.airdesk.airdesk_app.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class FacilityController {
     
 }
--- src/main/java/it/airdesk/airdesk_app/controller/GlobalController.java
+++ src/main/java/it/airdesk/airdesk_app/controller/GlobalController.java
@@ -1,5 +1,16 @@
 package it.airdesk.airdesk_app.controller;
 
-public class GlobalController {
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ControllerAdvice
+public class GlobalController { //most proper class to handle the index mapping, as a facade controller
     
+    @GetMapping("/")
+    public String getIndex() {
+        return "index.html";
+    }
+
+
+
 }
